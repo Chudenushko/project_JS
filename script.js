73d8550d953244bec5189a520ee6f2ca16ade4d3
Olena <chudenushko@gmail.com>
2024-01-18 19:49:38 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,118 @@
+// "use strict";
+
+
+// let number = 1;
+// number = 10;
+// console.log(number);
+
+
+
+// const a = 5000;
+// const b = 1000;
+
+// console.log("Ширина: " + a + ', довжина: ' + b);
+
+
+// const obj = {
+//   name: "John",
+//   age: 25,
+//   isMarried: false
+// };
+// console.log(obj["age"]);
+
+
+// const arr = ['a', 'b', 'c'];
+// const arrObj = {
+//   0: 'a',
+//   1: 'b',
+//   2: 'c',
+// }
+
+// arrObj.a = '12345';
+
+// console.log(arrObj[2]);
+// console.log(arrObj.a);
+// console.log(arrObj['a']);
+
+
+// const arr = ['a', 'b', 'c'];
+// const arrObj = {
+//   0: 'a',
+//   1: 'b',
+//   2: 'c'
+// }
+
+// arrObj['b'] = '1234567';
+
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+
+
+// const arr = ['a', 'b', 'c'];
+// const arrObj = {
+//   a: 'a',
+//   '1': 'b',
+//   2: 'c'
+// }
+// const b = 'b';
+// arrObj[b] = '12345678';
+
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+
+// alert('Hello!!')
+
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+
+// const answer = prompt("Are you 18 years old?", "45");
+// console.log(answer + 5);
+
+
+
+// const answers = [];
+
+// answers[0] = prompt('Як Вас звати?', '');
+// answers[1] = prompt('Яке Ваше прізвище?', '');
+// answers[2] = prompt('Скільки Вам років?', '');
+
+// document.write(answers);
+
+
+
+// const answers = [];
+// console.log(typeof(answers));
+
+
+
+
+// console.log('arr' + " - object");
+// console.log(4 + " - object");
+
+
+
+let incr = 10;
+let decr = 10;
+
+// incr++;
+// decr--;
+console.log(++incr);
+console.log(--decr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
